1438d2e260103177da8b188e3367adadc2bbb4d8
dorofeev-a-o <72602067+dorofeev-a-o@users.noreply.github.com>
2024-05-03 16:07:10 +0300

    Add files via upload

--- Algoritms.py
+++ Algoritms.py
@@ -1,8 +1,16 @@
-def f(x, y):
+def f3(x, y):
     if x == y:
         return 1
     if x > y:
         return 0
     else:
-        return f(x + 2, y) + f(x + 4, y)
-print(f(4, 22))
+        return f3(x + 2, y) + f3(x + 4, y)
+#print(f3(4, 22))
+
+def f2(x, y):
+   if x > y or x == 17:
+       return 0
+   if x == y:
+       return 1
+   return f2(x + 1, y) + f2(x + 2, y) + f2(x * 3, y)
+print(f2(3, 10) * f2(10, 25))
